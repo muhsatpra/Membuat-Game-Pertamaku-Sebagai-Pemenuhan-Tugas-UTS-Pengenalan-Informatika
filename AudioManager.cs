50e484b9bd0d0361bb10181fc53878d960009613
Muhammad Satrio Pratama <144855998+muhsatpra@users.noreply.github.com>
2023-12-27 08:46:46 +0700

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public MainVolume[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("Bgm");
+    }
+
+    public void PlayMusic(string name)
+    {
+        MainVolume s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("No BGM Sound");
+        }
+
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        MainVolume s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("No SFX Sound");
+        }
+
+        else
+        {
+            sfxSource.clip = s.clip;
+            sfxSource.Play();
+        }
+    }
+}
---
+++ MainVolume.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MainVolume
+{
+    public string name;
+    public AudioClip clip;
+}
